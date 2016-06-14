30fa176cfbefaeae4c32576889bbe1755a14b3b4
Leanhope <hans.lienhop@gmail.com>
2016-06-14 11:34:46 +0200

    code added

--- RGM.cpp
+++ RGM.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+
 #include <boost/foreach.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_utility.hpp>
@@ -8,113 +9,103 @@
 #include <boost/random/random_number_generator.hpp>
 #include <boost/graph/random.hpp>
 #include <boost/random.hpp>
-#include <time.h>
+#include <boost/progress.hpp>
+#include <chrono>
 #include <fstream>
-<<<<<<< HEAD
-=======
-//#include <omp.h>
->>>>>>> 5614ab8e4805b8faee2368b3d7359ed698168961
 
 using namespace boost;
 
 int main(int argc, char* argv[]) 
-{
+{  
   typedef adjacency_list <vecS, vecS, undirectedS> Graph;
   typedef graph_traits<Graph>::vertex_descriptor Vertex;
   typedef graph_traits<Graph>::vertices_size_type VertexIndex;
 
-  const int VERTEX_COUNT = 20;
-  Graph graph(VERTEX_COUNT);
+  int VERTEX_COUNT;
+  int EDGE_COUNT;
 
-  std::vector<VertexIndex> rank(num_vertices(graph));
-  std::vector<Vertex> parent(num_vertices(graph));
+  std::cout << "Number of vertices: ";
+  std::cin >> VERTEX_COUNT;
+  std::cout << "Number of edges: " ;
+  std::cin >> EDGE_COUNT;  
 
-  typedef VertexIndex* Rank;
-  typedef Vertex* Parent;
+		auto start = std::chrono::steady_clock::now();
 
-  disjoint_sets<Rank, Parent> ds(&rank[0], &parent[0]);
+		Graph graph(VERTEX_COUNT);
 
-  initialize_incremental_components(graph, ds);
-  incremental_components(graph, ds);
+		std::vector<VertexIndex> rank(num_vertices(graph));
+		std::vector<Vertex> parent(num_vertices(graph));
 
-  graph_traits<Graph>::edge_descriptor edge;
-  bool flag;
+		typedef VertexIndex* Rank;
+		typedef Vertex* Parent;
 
-  typedef component_index<VertexIndex> Components;
-<<<<<<< HEAD
+		disjoint_sets<Rank, Parent> ds(&rank[0], &parent[0]);
 
-  for(int i = 0; i < 10; i++)
-  {
-  	random::mt19937 gen{static_cast<std::uint32_t>(rand())};
-  	random::mt19937 gen1{static_cast<std::uint32_t>(rand())};
-	  Vertex a = random_vertex(graph, gen);
-	  Vertex b = random_vertex(graph, gen1);
-	
-	  boost::tie(edge, flag) = add_edge(a, b, graph);
-  	ds.union_set(a,b);
+		initialize_incremental_components(graph, ds);
+		incremental_components(graph, ds);
 
-    std::cout << i << std::endl;
-  	//Components components(parent.begin(), parent.end());
-   //  std::cout << a << " - " << b << std::endl;
-  	
-  	// if(components.size() == 1)
-  	// {
-  	// 	std::cout << "Graph fully connected after adding " << i << " edges." << std::endl << std::endl;;
-   //    std::ofstream outfile;
-   //    outfile.open("results.txt", std::ios_base::app);
-   //    outfile << i << "\n"; 
-  	// 	break;
-  	// } 
-  }
+		graph_traits<Graph>::edge_descriptor edge;
+		bool flag;
 
-  std::cout << std::endl;
-=======
-  
- // #pragma omp parallel for(4)
-  //{
-    for(int i = 0; i < 10000; i++)
-    {
-    	random::mt19937 gen{static_cast<std::uint32_t>(rand())};
-    	random::mt19937 gen1{static_cast<std::uint32_t>(rand())};
-  	  Vertex a = random_vertex(graph, gen);
-  	  Vertex b = random_vertex(graph, gen1);
-  	
-  	  boost::tie(edge, flag) = add_edge(a, b, graph);
-    	ds.union_set(a,b);
-     	incremental_components(graph, ds);
-    	Components components(parent.begin(), parent.end());
-      std::cout << a << " - " << b << std::endl;
-    	
-    	if(components.size() == 1)
-    	{
-    		std::cout << "Graph fully connected after adding " << i << " edges." << std::endl;
-        std::ofstream outfile;
-        outfile.open("results.txt", std::ios_base::app);
-        outfile << i << "\n"; 
-    		break;
-    	} 
-    }
-  //}
->>>>>>> 5614ab8e4805b8faee2368b3d7359ed698168961
-    
-   BOOST_FOREACH(Vertex current_vertex, vertices(graph)) {
-   std::cout << "representative[" << current_vertex << "] = " <<
-   ds.find_set(current_vertex) << std::endl;
-  }
+		typedef component_index<VertexIndex> Components;
 
-  std::cout << std::endl;
+		boost::progress_display show_progress(EDGE_COUNT);
 
-  Components components(parent.begin(), parent.end());
+		  for(int i = 0; i < EDGE_COUNT; i++)
+		  {
+		  	random::taus88 gen(rand());
+		  	random::taus88 gen1(rand());
+			  Vertex a = random_vertex(graph, gen);
+			  Vertex b = random_vertex(graph, gen1);
+			
+			  boost::tie(edge, flag) = add_edge(a, b, graph);
+			  if(ds.find_set(a) == ds.find_set(b))
+			  	{
+			  		std::cout << std::endl << "Cycle found after " << i << " edges." << std::endl;
+		  			std::ofstream outfile;
+					  outfile.open("results.txt", std::ios_base::app);
+					  outfile << "Edges to Cycle: " << i << std::endl;
+			  		break;
+			  	}
+		  	ds.union_set(a,b);
+		  	++show_progress;
+		    //std::cout << a << " - " << b << std::endl;
+		  	
+		  // 	if(components.size() == 1)
+		  // 	{
+		  // 		std::cout << "Graph fully connected after adding " << i << " edges." << std::endl;
+		  //     std::ofstream outfile;
+		  //     outfile.open("results.txt", std::ios_base::app);
+		  //     outfile << i << "\n"; 
+		  // 		break;
+		  // 	} 
+		  }
+		  
+		// BOOST_FOREACH(Vertex current_vertex, vertices(graph)) {
+		// std::cout << "representative[" << current_vertex << "] = " <<
+		// ds.find_set(current_vertex) << std::endl;
+		//}
 
-  BOOST_FOREACH(VertexIndex current_index, components) {
-    std::cout << "component " << current_index << " contains: ";
+		std::cout << std::endl;
 
-    BOOST_FOREACH(VertexIndex child_index,
-                  components[current_index]) {
-      std::cout << child_index << " ";
-    }
+		Components components(parent.begin(), parent.end());
 
-    std::cout << std::endl;
-  }
-  return (0);
+		// BOOST_FOREACH(VertexIndex current_index, components) {
+		//   std::cout << "component " << current_index << " contains: ";
+
+		//   BOOST_FOREACH(VertexIndex child_index,
+		//                 components[current_index]) {
+		//     std::cout << child_index << " ";
+		//   }
+
+		//   std::cout << std::endl;
+		// }
+
+		std::chrono::duration<double> diff = std::chrono::steady_clock::now() - start;
+
+		std::cout << diff.count() << " seconds needed." << std::endl;
+		// std::ofstream outfile;
+		//   outfile.open("results.txt", std::ios_base::app);
+		//   outfile << "V: " << VERTEX_COUNT << " E: " << EDGE_COUNT << " TiS " << diff.count() <<"\n"; 
+		return (0);
 }
---
+++ build/CMakeFiles/RGM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RGM.dir/RGM.cpp.o"
+  "RGM.pdb"
+  "RGM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RGM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
